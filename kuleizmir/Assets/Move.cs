84b033b600d5868816ae871b0cfde5744c03929d
Furkan Selek <furkanselek@outlook.com>
2021-10-30 05:15:54 +0300

    fix jump

--- kuleizmir/Assets/Move.cs
+++ kuleizmir/Assets/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour
     public float moveforce = 10f;
     public float jumpforce = 22f;
     public float movmentX;
+    //public bool isGrounded;
     private Rigidbody2D myBody;
     private SpriteRenderer sf;
     private Animator anim;
@@ -24,17 +25,24 @@ public class Move : MonoBehaviour
     void Update()
     {
         PlayerMoveKeyboard();
+        PlayerJump();
     }
-
     void PlayerMoveKeyboard()
     {
         movmentX = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movmentX, 0f, 0f) * moveforce * Time.deltaTime;
     }
 
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            myBody.AddForce(new Vector2(0f, jumpforce), ForceMode2D.Impulse);
+        }
+    }
     void AnimatePlayer()
     {
-        if(movmentX > 0)
+        if (movmentX > 0)
         {
             anim.SetBool(WALK_ANIMATION, true);
             sf.flipX = false;
@@ -50,3 +58,18 @@ public class Move : MonoBehaviour
         }
     }
 }
+
+
+
+//    private void OnCollisionEnter2D(Collision2D collision)
+//    {
+//        if (collision.gameObject.CompareTag("Ground"))
+//        {
+//            isGrounded = true;
+//        }
+//        else
+//        {
+//            isGrounded = false;
+//        }
+//    }
+//}
